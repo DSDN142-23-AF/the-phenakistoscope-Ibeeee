6561eb333b3620bc06dfa69250d01c97b8e5edde
IvyXue <xueivy@myvuw.ac.nz>
2024-01-12 13:01:34 +1300

    I Forgot How To Commit

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,4 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 18;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
@@ -6,30 +6,55 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.set_direction(CCW);
+  pScope.load_image("Water_2", "png");
+  pScope.load_image("Water", "png");
+  pScope.load_image("Water_3", "png");
+  pScope.load_image("Fishes", "png");
+  pScope.load_image("Fishes_2", "png");
 }
 
 function setup_layers(pScope){
-
+  
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
-
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  
+  var layer1 = new PLayer(bubbles);
+  layer1.mode( SWIRL (4) );
+  layer1.set_boundary( 20, 1000 );
 
   var layer2 = new PLayer(squares);
   layer2.mode( RING );
   layer2.set_boundary( 0, 400 );
+
+  var layer3 = new PLayer(imagedraw);
+  layer3.mode( RING )
+  layer3.set_boundary( 0, 1000 );
+
+  var layer4 = new PLayer(boarder);
+  layer4.mode( SWIRL (5) );
+  layer4.set_boundary( 125, 100 );
+
 }
 
-function faces(x, y, animation, pScope){
+function imagedraw (x, y, animation, pScope){
   
-  scale(animation.frame*2);
+  pScope.draw_image("Water",animation.wave()*60,300);
+  pScope.draw_image("Water_2",animation.wave()*80,400);
+  pScope.draw_image("Water_3",animation.wave()*100,500);
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  pScope.draw_image("Fishes",animation.wave()*40,630,);
+  pScope.draw_image("Fishes_2",animation.wave()*30,750,800,10);
+}
+
+function bubbles(x, y, animation, pScope){
+
+  noStroke();
+  scale(animation.frame*9);
+  fill(45, 178, 220);
+  ellipse(0,0,100,100);
+
+  fill(255, 255, 255, 20);
+  ellipse(x,500,150,100);
 
 }
 
@@ -39,11 +64,22 @@ function squares(x, y, animation, pScope){
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
+  
+  
+  strokeWeight(1);
+  stroke(119, 210, 228, 127);
+  fill(119, 210, 228, 180, 127);
+  arc(x,y,200,800,backgroundArcStart,backgroundArcEnd); 
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+}
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+function boarder(x, y, animation, pScope){
 
+  let angleOffset = (360 / SLICE_COUNT) / 2
+  let backgroundArcStart = 180 - angleOffset;
+  let backgroundArcEnd = 50 + angleOffset;
+
+  noStroke();
+  fill(31, 33, 85, 127);
+  arc(700,1000,800,500,backgroundArcStart,backgroundArcEnd);
 }
